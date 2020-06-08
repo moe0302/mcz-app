0763504820517132cc0d4b5631df46b06a552093
moe0302 <nomura@griffin-net.co.jp>
2020-06-08 10:56:50 +0900

    Project's Initial Commit

---
+++ www/quiz/quiz_main.js
@@ -0,0 +1,88 @@
+{
+  const question = document.getElementById('question');
+  const choices = document.getElementById('choices');
+  const btn = document.getElementById('btn');
+  const result = document.getElementById('result');
+  const scoreLabel = document.querySelector('#result > p');
+
+  const quizSet = shuffle([
+    {q: '高城れにの誕生日は？', c: ['1993年6月21日', '1995年6月4日', '1996年6月11日']},
+    {q: '高城れにのメンバーカラーは？', c: ['紫', '赤', '黄']},
+    {q: '高城れにの飼ってる猫の名前は？', c: ['ゼット', 'イロハ', 'ピーチ']},
+    {q: '玉井詩織の誕生日は？', c: ['ゼット', 'イロハ', 'ピーチ']},
+    {q: '玉井詩織の誕生日は？', c: ['ゼット', 'イロハ', 'ピーチ']},
+    {q: '玉井詩織の誕生日は？', c: ['ゼット', 'イロハ', 'ピーチ']},
+    {q: '高城れにの飼ってる猫の名前は？', c: ['ゼット', 'イロハ', 'ピーチ']},
+    {q: '高城れにの飼ってる猫の名前は？', c: ['ゼット', 'イロハ', 'ピーチ']},
+    {q: '高城れにの飼ってる猫の名前は？', c: ['ゼット', 'イロハ', 'ピーチ']},
+    {q: '高城れにの飼ってる猫の名前は？', c: ['ゼット', 'イロハ', 'ピーチ']},
+  ]);
+  let currentNum = 0;
+  let isAnswered;
+  let score = 0;
+
+  function shuffle(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[j], arr[i]] = [arr[i], arr[j]];
+    }
+    return arr;
+  }
+
+  function checkAnswer(li) {
+    if (isAnswered) {
+      return;
+    }
+    isAnswered = true;
+
+    if (li.textContent === quizSet[currentNum].c[0]) {
+      li.classList.add('correct');
+      score++;
+    } else {
+      li.classList.add('wrong');
+    }
+
+    btn.classList.remove('disabled');
+  }
+
+  function setQuiz() {
+    isAnswered = false;
+
+    question.textContent = quizSet[currentNum].q;
+
+    while (choices.firstChild) {
+      choices.removeChild(choices.firstChild);
+    }
+
+    const shuffledChoices = shuffle([...quizSet[currentNum].c]);
+    shuffledChoices.forEach(choice => {
+      const li = document.createElement('li');
+      li.textContent = choice;
+      li.addEventListener('click', () => {
+        checkAnswer(li);
+      });
+      choices.appendChild(li);
+    });
+
+    if (currentNum === quizSet.length - 1) {
+      btn.textContent = 'Show Score';
+    }
+  }
+
+  setQuiz();
+
+  btn.addEventListener('click', () => {
+    if (btn.classList.contains('disabled')) {
+      return;
+    }
+    btn.classList.add('disabled');
+
+    if (currentNum === quizSet.length - 1) {
+      scoreLabel.textContent = `Score: ${score} / ${quizSet.length}`;
+      result.classList.remove('hidden');
+    } else {
+      currentNum++;
+      setQuiz();
+    }
+  });
+}
