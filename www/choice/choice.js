4488e7e89a52792ca7b27642f434c8a05085a76c
moe0302 <nomura@griffin-net.co.jp>
2020-06-19 18:49:15 +0900

    クイズの問題の追加レイアウトの調整

--- www/choice/choice.js
+++ www/choice/choice.js
@@ -219,7 +219,7 @@
     let todayNum = passedDay % (musicSet.length);
     console.log(todayNum);
 
-    // let todayNum = 27;
+    // let todayNum = 10;
 
     // 曲の情報を埋め込む
     jacket.setAttribute('src', musicSet[todayNum].j);
